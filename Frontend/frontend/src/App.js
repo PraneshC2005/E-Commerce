635e051858862ba84a3a2c1e9edb86a91da2e2cb
Pranesh Chandrasekaran <125540554+PraneshC2005@users.noreply.github.com>
2025-04-14 19:50:17 +0530

    shopCategory commit

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -7,6 +7,9 @@ import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
 import Footer from './Components/Footer/Footer';
+import men_bannner from './Components/Assets/banner_mens.png';
+import women_bannner from './Components/Assets/banner_women.png';
+import kid_bannner from './Components/Assets/banner_kids.png';
 function App() {
   return (
     <div>
@@ -14,9 +17,9 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Shop/>}/>
-        <Route path="/mens" element={<ShopCategory category="men"/>}/>
-        <Route path="/womens" element={<ShopCategory category="women"/>}/>
-        <Route path="/kids" element={<ShopCategory category="kid"/>}/>
+        <Route path="/mens" element={<ShopCategory banner={men_bannner} category="men"/>}/>
+        <Route path="/womens" element={<ShopCategory banner={women_bannner} category="women"/>}/>
+        <Route path="/kids" element={<ShopCategory banner={kid_bannner} category="kid"/>}/>
         <Route path="/product" element={<Product/>}>
          <Route path=":productId" element={<Product/>}/>
         </Route>
--- Frontend/frontend/src/Pages/LoginSignup.jsx
+++ Frontend/frontend/src/Pages/LoginSignup.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import './CSS/LoginSignup.css';
 const LoginSignup = () =>{
     return(
-        <div>
-
+        <div className='loginsignup'>
+            <div className="loginsignup-container">
+                <h1>Sign Up</h1>
+                <div className="loginsignup-fields">
+                    <input type="text" placeholder='Your Name'/>
+                    <input type="Email" placeholder='Your Email'/>
+                    <input type="password" placeholder='Your Password'/>
+                </div>
+                <button>Continue</button>
+                <p className='loginsignup-login'>Already have an account? <span>Login</span></p>
+                <div className="loginsignup-agree">
+                    <input type="checkbox" name='' id=''/>
+                    <p>Agree to condition of terms of service</p>
+                </div>
+            </div>
         </div>
     )
 };
